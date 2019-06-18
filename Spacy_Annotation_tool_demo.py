43263d4888ca59fac3de34a549f5f00bc0d1112a
Heisenbug03 <33387666+Heisenbug03@users.noreply.github.com>
2019-06-18 11:20:23 +0530

    Add files via upload

---
+++ Spacy_Annotation_tool_demo.py
@@ -0,0 +1,313 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import spacy
+from spacy.lang.en.examples import sentences
+#nlp = spacy.load('en_core_web_en')
+nlp = spacy.load('en')
+
+
+# In[2]:
+
+
+
+doc = nlp(u'''For multi-modal, we need to add zoom and brightness functionality for LIDAR, to match with the functionality developed for the images. It will use the same sliders, so no GUI is necessary.''')
+
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+
+# In[3]:
+
+
+
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+
+
+# In[4]:
+
+
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+
+
+# In[5]:
+
+
+# new entity label
+LABEL = 'ACTION'
+
+
+# In[6]:
+
+
+TRAIN_DATA = [
+    ("using the previous work to spread search agains the api by keys, use search by mission id key", {
+        'entities': [(28, 34, 'ACTION')]
+    }),
+
+    ("For multi-modal, we need to add search and brightness functionality for LIDAR, to match with the functionality developed for the images. It will use the same sliders, so no GUI is necessary.", {
+        'entities': [(24,35, 'Action')]
+    }),
+
+    ("we observed that when selecting the search action and then hitting cancel button, it was toggling the whole mission to cancelled state. This button should just close the modal and abort the fail action", {
+        'entities': [(30, 37, 'Action')]
+    }),
+
+    ("after search a mission, there is a new table row added but it is difficult to understand which one this is so we should have a small highlight on this row for a second or 2 that fades away on the row that was added", {
+        'entities': [(6, 11, 'Action')]
+    }),
+
+    ("WO which are search in multi modal are not displaying duration/frames in ADA/A.", {
+        'entities': [(10, 16, 'Action')]
+    }),
+
+    ("In DEV, from search tag application, selecting on 3D-Other does not create a Work Order.", {
+        'entities': [(9, 15, 'Action')]
+    })
+]
+
+
+# In[7]:
+
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    new_model_name=("New model name for model meta.", "option", "nm", str),
+    output_dir=("Optional output directory", "option", "o", Path),
+    n_iter=("Number of training iterations", "option", "n", int))
+
+
+def main(model=None, new_model_name='Action', output_dir=None, n_iter=20):
+    """Set up the pipeline and entity recognizer, and train the new entity."""
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank('en')  # create blank Language class
+        print("Created blank 'en' model")
+    # Add entity recognizer to model if it's not in the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner)
+    # otherwise, get it, so we can add labels to it
+    else:
+        ner = nlp.get_pipe('ner')
+
+    ner.add_label(LABEL)   # add new entity label to entity recognizer
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        # Note that 'begin_training' initializes the models, so it'll zero out
+        # existing entity types.
+        optimizer = nlp.entity.create_optimizer()
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            for text, annotations in (TRAIN_DATA):
+                nlp.update([text], [annotations], sgd=optimizer, drop=0.35,
+                           losses=losses)
+            print(losses)
+
+    # test the trained model
+    test_text = 'how to search in Google '
+    doc = nlp(test_text)
+    print("Entities in '%s'" % test_text)
+    for ent in doc.ents:
+        print(ent.label_, ent.text)
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.meta['name'] = new_model_name  # rename model
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        doc2 = nlp2(test_text)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
+
+
+if __name__ == '__main__':
+    plac.call(main)
+
+
+In[8]:
+
+
+Run our Function
+main()
+
+
+In[ ]:
+
+
+## plac is wrapper for argparser 
+@plac.annotations(
+    model= ("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    new_model_name=("New model name for model meta.", "option", "nm", str),
+    output_dir=("/root/root", "option", "o", Path),
+    n_iter= (5, "option", "n",input))
+def main(model=None, new_model_name='action', output_dir=("/root/root"), n_iter=10):
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank('en')  # create blank Language class
+        print("Created blank 'en' model")
+    # Add entity recognizer to model if it's not in the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner)
+    # otherwise, get it, so we can add labels to it
+    else:
+        ner = nlp.get_pipe('ner')
+
+    ner.add_label(LABEL)   # add new entity label to entity recognizer
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        # Note that 'begin_training' initializes the models, so it'll zero out
+        # existing entity types.
+        optimizer = nlp.entity.create_optimizer()
+
+
+In[ ]:
+
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(TRAIN_DATA, size=compounding(4., 32., 1.001))
+        for batch in batches:
+            texts, annotations = zip(*batch)
+            nlp.update(texts, annotations, sgd=optimizer, drop=0.35,
+            losses=losses)
+        print('Losses', losses)
+
+
+In[ ]:
+
+
+    #ner.add_label(LABEL)   # add new entity label to entity recognizer
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        # Note that 'begin_training' initializes the models, so it'll zero out
+        # existing entity types.
+        optimizer = nlp.entity.create_optimizer()
+
+
+In[ ]:
+
+
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        # Note that 'begin_training' initializes the models, so it'll zero out
+        # existing entity types.
+        optimizer = nlp.entity.create_optimizer()
+
+
+In[ ]:
+
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    new_model_name=("New model name for model meta.", "option", "nm", str),
+    output_dir=("Optional output directory", "option", "o", Path),
+    n_iter=("Number of training iterations", "option", "n", int))
+def main(model=None, new_model_name='action', output_dir=None, n_iter=10):
+    """Set up the pipeline and entity recognizer, and train the new entity."""
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank('en')  # create blank Language class
+        print("Created blank 'en' model")
+    # Add entity recognizer to model if it's not in the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner)
+    # otherwise, get it, so we can add labels to it
+    else:
+        ner = nlp.get_pipe('ner')
+
+    ner.add_label(LABEL)   # add new entity label to entity recognizer
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        # Note that 'begin_training' initializes the models, so it'll zero out
+        # existing entity types.
+        optimizer = nlp.entity.create_optimizer()
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(TRAIN_DATA, size=compounding(4., 32., 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(texts, annotations, sgd=optimizer, drop=0.35,
+                           losses=losses)
+            print('Losses', losses)
+
+    # test the trained model
+    test_text = 'Do you like horses?'
+    doc = nlp(test_text)
+    print("Entities in '%s'" % test_text)
+    for ent in doc.ents:
+        print(ent.label_, ent.text)
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.meta['name'] = new_model_name  # rename model
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        doc2 = nlp2(test_text)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
+
+
+if __name__ == '__main__':
+    plac.call(main)
+
